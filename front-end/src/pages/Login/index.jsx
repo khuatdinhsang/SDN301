ad9c0c5de691289736bc47d5f59c7ab363f935d5
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2023-09-29 10:47:55 +0700

    api google fe

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,121 +1,153 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router'
-import { toast } from 'react-toastify'
-import { loginAccount, logout } from '../../actions/accountAction'
-import './Login.scss'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { loginAccount, logout } from "../../actions/accountAction";
+import "./Login.scss";
+import { Link as MuiLink } from "@mui/material";
+import { getGoogleUrl } from "../../utils.js";
 
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const action = logout();
+    dispatch(action);
+    axios
+      .post("/api/account/logout")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-function Login(){
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    useEffect(() => {
-        const action = logout();
-        dispatch(action);
-        axios
-        .post("/api/account/logout")
-        .then((res) => {
-            console.log(res.data);
-        })
-        .catch(err => console.log(err))
-    },[])
-
-    const handleLogin = () => {
-        const userLogin = {
-            username: username,
-            password: password
+  const handleLogin = () => {
+    const userLogin = {
+      username: username,
+      password: password,
+    };
+    axios
+      .post("/api/account/login", userLogin)
+      .then((res) => {
+        // console.log(res.data);
+        if (res.data.status === "OK") {
+          const action = loginAccount(userLogin);
+          dispatch(action);
+          toast.success("Login successfully");
+          navigate("/");
+        } else {
+          toast.error("Username or Password is not correct!");
         }
-        axios
-        .post("/api/account/login", userLogin)
-        .then(
-            (res) => {
-                // console.log(res.data);
-                if(res.data.status === "OK"){
-                    const action = loginAccount(userLogin)
-                    dispatch(action)
-                    toast.success("Login successfully")
-                    navigate("/")
-                }else{
-                    toast.error("Username or Password is not correct!")
-                }
-            }
-        )
-        .catch(err => toast(err))
-    }
-
-
-    return(
-         <div className="container">
-            <div className="loginContent">
-                <div className="leftContent">
-                <h1>Sign In</h1>
-                <img src="https://daizdje8zyv90.cloudfront.net/wp-content/uploads/2016/12/Grilled-Clams-with-Charred-Jalapen%CC%83o-Basil-Butter-Half-Baked-Harvest.jpg" alt=''/>
-                <h2>Privacy policy {"&"} Term of service</h2>
-                </div>
-                <div className="rightContent">
-                <div className="input">
-                    <label htmlFor="mail">Username: </label>
-                    <input
-                    placeholder="Enter Username "
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    id="mail"
-                    />
-                </div>
-                <div className="input">
-                    <label htmlFor="password">Password:</label>
-                    <input
-                    placeholder="Password"
-                    type={"password"}
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    id="password"
-                    />
-                </div>
-                <p
-                    style={{
-                    color: "#f9004d",
-                    textAlign: "left",
-                    fontSize: "12px",
-                    display: "none",
-                    }}
-                >
-                    Incorrect Email or Password
-                </p>
-                <p
-                    style={{
-                    color: "#f9004d",
-                    textAlign: "left",
-                    fontSize: "12px",
-                    display: "none",
-                    }}
-                >
-                    Account is Blocked
-                </p>
-                <div className="handle">
-                    <button onClick={() => handleLogin()} >Login</button>
-                    <i
-                    className="remember"
-                    style={{textAlign: "center"}} 
-                    >Change Password</i>
-                    <i className="remember" >Remember Password</i>
-                </div>
-                <div className="register">
-                    <b >You don't have an account? </b>
-                    <p
-                    style={{ cursor: "pointer" }}
-                    onClick={() => {navigate("/signUp")}}
-                    >
-                    Register
-                    </p>
-                </div>
-                </div>
-            </div>
+      })
+      .catch((err) => toast(err));
+  };
+  // const test = () => {
+  //   axios.post("/auth/google").then((res) => {
+  //     console.log(res.data);
+  //   });
+  // };
+  return (
+    <div className="container">
+      <div className="loginContent">
+        <div className="leftContent">
+          <h1>Sign In</h1>
+          <img
+            src="https://daizdje8zyv90.cloudfront.net/wp-content/uploads/2016/12/Grilled-Clams-with-Charred-Jalapen%CC%83o-Basil-Butter-Half-Baked-Harvest.jpg"
+            alt=""
+          />
+          <h2>Privacy policy {"&"} Term of service</h2>
+        </div>
+        <div className="rightContent">
+          <div className="input">
+            <label htmlFor="mail">Username: </label>
+            <input
+              placeholder="Enter Username "
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              id="mail"
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="password">Password:</label>
+            <input
+              placeholder="Password"
+              type={"password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+            />
+          </div>
+          <p
+            style={{
+              color: "#f9004d",
+              textAlign: "left",
+              fontSize: "12px",
+              display: "none",
+            }}
+          >
+            Incorrect Email or Password
+          </p>
+          <p
+            style={{
+              color: "#f9004d",
+              textAlign: "left",
+              fontSize: "12px",
+              display: "none",
+            }}
+          >
+            Account is Blocked
+          </p>
+          <div className="handle">
+            <button onClick={() => handleLogin()}>Login</button>
+            <i className="remember" style={{ textAlign: "center" }}>
+              Change Password
+            </i>
+            <i className="remember">Remember Password</i>
+          </div>
+          <div className="register">
+            <b>You don't have an account? </b>
+            <p
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                navigate("/signUp");
+              }}
+            >
+              Register
+            </p>
+          </div>
+          <MuiLink
+            href={"http://localhost:3001/auth/google"}
+            sx={{
+              backgroundColor: "#f5f6f7",
+              borderRadius: 1,
+              py: "0.6rem",
+              columnGap: "1rem",
+              textDecoration: "none",
+              color: "#393e45",
+              cursor: "pointer",
+              fontWeight: 500,
+              "&:hover": {
+                backgroundColor: "#fff",
+                boxShadow: "0 1px 13px 0 rgb(0 0 0 / 15%)",
+              },
+            }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            Google
+          </MuiLink>
+          {/* <button onClick={test}>Goi di</button> */}
+          <form action="http://localhost:3001/auth/google" method="post">
+            <input type="submit" value="Press to log in" />
+          </form>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Login
+export default Login;
