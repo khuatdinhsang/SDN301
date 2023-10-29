f31b94c73af2a29800fa3ecfeda9f710ebfa4818
SeaW222 <nguyenhaidong2582002@gmail.com>
2023-10-29 07:46:25 +0700

    create shipping page, chat shipping

--- front-end/src/components/Layout/component/Header/index.jsx
+++ front-end/src/components/Layout/component/Header/index.jsx
@@ -20,29 +20,29 @@ const Header = () => {
   const [isLoading, setIsLoading] = useState(true)
 
   const dispatch = useDispatch()
- 
-  const handleLogout = () =>{
+
+  const handleLogout = () => {
     setIsLoading(false)
     const action = logout();
     dispatch(action);
     axios
-    .post("/api/account/logout")
-    .then((res) => {
-      if(res.data.status === 'OK'){
-        navigate("/")
-        toast.success("Logout successfully!")
-        setIsLoading(true)
-      }else{
-        toast.error("Somethings Wrong!")
-        setIsLoading(true)
-      } 
-    })
-    .catch(err => console.log(err))
-    
+      .post("/api/account/logout")
+      .then((res) => {
+        if (res.data.status === 'OK') {
+          navigate("/")
+          toast.success("Logout successfully!")
+          setIsLoading(true)
+        } else {
+          toast.error("Somethings Wrong!")
+          setIsLoading(true)
+        }
+      })
+      .catch(err => console.log(err))
+
   }
 
 
-  
+
   console.log(account);
   const handleAdmin = () => {
     account.role == 1 ? navigate('/admin/general') : navigate('/admin/productsManager')
@@ -58,48 +58,48 @@ const Header = () => {
   useEffect(() => {
     setClassActive(location.pathname)
   }, [location.pathname])
-      return (<React.Fragment>
-        {isLoading ? 
-        <div className="contain">
-            <div className="nav">
-                <div className="logo">
-                  <h1 onClick={() => navigate("/")}>Hola<b>Food</b></h1>
-                </div>
-                <ul>
-                  <li className={classActive === '/' ? "active home" : "home"} onClick={() => navigate("/")}><span>Home</span></li>
-                  <li className={classActive.includes('menu') ? "active menu" : "menu"} onClick={() => navigate("/menu")}><span>Menu</span></li>
-                  <li className="services"><span>Service</span></li>
-                  <li className="about"><span>About Us</span></li>
-                  <li className="gallery"><span>Gallery</span></li>
-                </ul>
-                <div className="action">
-                      {account?.username !== undefined?
-                      <>
-                        <span className="cartIcon" onClick={() => navigate('/cart')}><ShoppingCartIcon className="shoppingCart"/>
+  return (<React.Fragment>
+    {isLoading ?
+      <div className="contain">
+        <div className="nav">
+          <div className="logo">
+            <h1 onClick={() => navigate("/")}>Hola<b>Food</b></h1>
+          </div>
+          <ul>
+            <li className={classActive === '/' ? "active home" : "home"} onClick={() => navigate("/")}><span>Home</span></li>
+            <li className={classActive.includes('menu') ? "active menu" : "menu"} onClick={() => navigate("/menu")}><span>Menu</span></li>
+            <li className="services" onClick={() => navigate('/shipper')}><span>Service</span></li>
+            <li className="about"><span>About Us</span></li>
+            <li className="gallery"><span>Gallery</span></li>
+          </ul>
+          <div className="action">
+            {account?.username !== undefined ?
+              <>
+                <span className="cartIcon" onClick={() => navigate('/cart')}><ShoppingCartIcon className="shoppingCart" />
 
-                        <span className="dot">{cartList?.length}</span>
+                  <span className="dot">{cartList?.length}</span>
 
-                      </span>
-                      <span className="usernameHeader" onClick={handleProductClick}>Hello, {account?.username}</span>
-                     {(account?.role !== 4 && account?.role !== 3) ? <button className="signIn" onClick={() => {
-                        handleAdmin();
-                      }}>Dashboard</button>:"" }
-                      <button className="signUp" onClick={() => {
-                        handleLogout();
-                      }}>Logout</button>
-                    </> :
-                    <>
-                      <span className="cartIcon" onClick={() => navigate('/cart')}><ShoppingCartIcon className="shoppingCart" /></span>
-                      <button className="signIn" onClick={() => navigate("/signUp")}>Sign Up</button>
-                      <button className="signUp" onClick={() => navigate("/login")}><span>Sign In <LoginIcon className="loginIcon" /></span></button>
-                    </>}
+                </span>
+                <span className="usernameHeader" onClick={handleProductClick}>Hello, {account?.username}</span>
+                {(account?.role !== 4 && account?.role !== 3) ? <button className="signIn" onClick={() => {
+                  handleAdmin();
+                }}>Dashboard</button> : ""}
+                <button className="signUp" onClick={() => {
+                  handleLogout();
+                }}>Logout</button>
+              </> :
+              <>
+                <span className="cartIcon" onClick={() => navigate('/cart')}><ShoppingCartIcon className="shoppingCart" /></span>
+                <button className="signIn" onClick={() => navigate("/signUp")}>Sign Up</button>
+                <button className="signUp" onClick={() => navigate("/login")}><span>Sign In <LoginIcon className="loginIcon" /></span></button>
+              </>}
 
-                </div>
-              </div>
-            
-          </div>:<Loading/>}
+          </div>
+        </div>
 
-    </React.Fragment>);
+      </div> : <Loading />}
+
+  </React.Fragment>);
 };
 
 export default Header;
